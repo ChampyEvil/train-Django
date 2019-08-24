60480af9c4d000547a620dfc7807a566ac5a62d7
Thirawuth <champ@odds.team>
2019-08-24 16:08:37 +0700

    add post and post list page

--- istagram/istagram/settings.py
+++ istagram/istagram/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django.contrib.humanize'
+    'django.contrib.humanize',
+    'posts'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [f'{BASE_DIR}/static']
+
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
